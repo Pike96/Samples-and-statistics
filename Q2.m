bd7dbec93e0ff068317887bae77f20dc86a873cf
Yucheng Lin <Pike96@users.noreply.github.com>
2017-01-24 14:53:04 -0800

    Finish Q2a for n=100000 with notes

--- Q2.m
+++ Q2.m
@@ -1,5 +1,10 @@
-n=100000;
-X=rand(1,n);
-Y=circshift(X,1,2);
-Y(1)=0;
-cov(X,Y)
+n=100000;               % n is the sequence length
+X=rand(1,n);            % X is X_k
+Y=circshift(X,1,2);     % Y is X_k+1 by shift array circularly first
+Y(1)=0;                 % Set the first element to 0 to get X_k+1
+C=cov(X,Y);             % Get covariance matrix
+
+disp(['The sample mean is: ',num2str(C(2,1))]); % Display Cov(X_k,X_k+1)
+
+% Xk and Xk+1 are correlated because cov... is never to be 0.
+% They are not independent but tends to independent when n is big.

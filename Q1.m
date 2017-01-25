f9d5b8e6736e20989b6530960ac22c6a6ec907e8
Yucheng Lin <Pike96@users.noreply.github.com>
2017-01-25 12:12:23 -0800

    Final edit

--- Q1.m
+++ Q1.m
@@ -7,7 +7,8 @@ for i = 1:n
     X = [X 5*rand() - 3];   
 end
 
-hist(X,100);
+hist(X)
+title(['n=',num2str(n)])
 
 disp(['The sample mean is: ',num2str(mean(X))]);
 disp(['The sample variance is: ',num2str(var(X))]);
@@ -17,7 +18,7 @@ S = sort(bootstrp(n,@std,X));
 
 % Get index of 2.5% point and 97.5% point
 disp(['The bootstrap confidence interval for sample mean is: [',...
-    num2str(M(round(n*0.025))),',',num2str(M(round(n*0.975))),']']);
+    num2str(M(ceil(n*0.025))),',',num2str(M(floor(n*0.975))),']']);
 disp(['The bootstrap confidence interval ',...
     'for sample standard deviation is: [',...
-    num2str(S(round(n*0.025))),',',num2str(S(round(n*0.975))),']']);
+    num2str(S(ceil(n*0.025))),',',num2str(S(floor(n*0.975))),']']);

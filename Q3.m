2886e7fe9628141ab69ce3f800eaedec59228b84
Yucheng Lin <Pike96@users.noreply.github.com>
2017-01-25 12:12:58 -0800

    Final edit

--- Q3.m
+++ Q3.m
@@ -3,6 +3,7 @@ M = 10;
 A = randi([0 M-1],1,n);     % A is the randaom number from 0,1,...,9
 
 hist(A)
+title(['n=',num2str(n)])
 
 X = hist(A);
 X_theo = repmat(n/M,1,M);   % Expected number of samples
@@ -10,11 +11,11 @@ ChisquaredTest = sum((X-X_theo).^2./X_theo);
 ChisquaredThreshold_95 = chi2inv(0.95,M-1);
 
 disp(['ChisquaredTest = ',num2str(ChisquaredTest), ...
-    ',	ChisquaredTest = ',num2str(ChisquaredThreshold_95)]);
+    ',	ChisquaredThreshold_95 = ',num2str(ChisquaredThreshold_95)]);
 
 X2 = [X(2:10) 0];           % The data in the range 1,2,...,10
 ChisquaredTest = sum((X2-X_theo).^2./X_theo);
 ChisquaredThreshold_95 = chi2inv(0.95,M-1);
 
 disp(['ChisquaredTest = ',num2str(ChisquaredTest), ...
-    ',	ChisquaredTest = ',num2str(ChisquaredThreshold_95)]);
+    ',	ChisquaredThreshold_95 = ',num2str(ChisquaredThreshold_95)]);

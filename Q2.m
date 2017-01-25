af4bf4fae1c2e30872158078a23af5edd096dc2c
Yucheng Lin <Pike96@users.noreply.github.com>
2017-01-24 17:19:34 -0800

    Small edit/Format

--- Q2.m
+++ Q2.m
@@ -1,24 +1,24 @@
-n=100000;                   % n is the sequence length
-Xk=rand(1,n);               % Xk is X_k
-Xk1=circshift(Xk,1,2);      % Xk1 is X_k+1 by shift array circularly first
-Xk1(1)=0;                   % Set the first element to 0 to get X_k+1
-Cov_a=cov(Xk,Xk1)           % Get covariance matrix
+n = 100000;                   % n is the sequence length
+Xk = rand(1,n);               % Xk is X_k
+Xk1 = circshift(Xk,1,2);      % Xk1 is X_k + 1 by shift array circularly first
+Xk1(1) = 0;                   % Set the first element to 0 to get X_k + 1
+Cov_a = cov(Xk,Xk1)           % Get covariance matrix
 
-disp(['Cov[X_k, X_k+1] is: ',num2str(Cov_a(2,1))]); % Display Cov(X_k,X_k+1)
+disp(['n = 100000, Cov[X_k, X_k + 1] is: ',num2str(Cov_a(2,1))]); % Display Cov(X_k,X_k + 1)
 
-% Xk and Xk+1 are correlated because cov... is never to be 0.
+% Xk and Xk + 1 are correlated because cov... is never to be 0.
 % They are not independent but tends to be independent when n is big.
 
-Yk=[];                      % Create Yk
-Yk(1)=Xk(1);
-Yk(2)=Xk(2)-2*Xk(1);
-Yk(3)=Xk(3)-2*Xk(2)+0.5*Xk(1);
-for k=4:n
-    Yk(k)=Xk(k)-2*Xk(k-1)+0.5*Xk(k-2)-Xk(k-3);
+Yk = [];                      % Create Yk
+Yk(1) = Xk(1);
+Yk(2) = Xk(2) - 2*Xk(1);
+Yk(3) = Xk(3) - 2*Xk(2) + 0.5*Xk(1);
+for k = 4:n
+    Yk(k) = Xk(k) - 2*Xk(k - 1) + 0.5*Xk(k - 2) - Xk(k - 3);
 end
 
-Cov_b=cov(Xk,Yk)
+Cov_b = cov(Xk,Yk)
 
-disp(['Cov[X_k, Y_k] is: ',num2str(Cov_b(2,1))]); % Display Cov(X_k,Y_k)
+disp(['n = 100000, Cov[X_k, Y_k] is: ',num2str(Cov_b(2,1))]); % Display Cov(X_k,Y_k)
 
 % Correlated. Cov(X_k,Y_k) is almost equal to Cov(X_k,X_k)

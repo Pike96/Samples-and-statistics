3cd79c9b4a459daac87904fde9c2e1dad6473a1f
Yucheng Lin <Pike96@users.noreply.github.com>
2017-01-24 17:18:23 -0800

    Finish Q3b and Q3c with notes

--- Q3.m
+++ Q3.m
@@ -1,6 +1,20 @@
-n=1000;                 % n is experiment times
-M=10;
-A=randi([0 M-1],1,n);
+n = 10000;                % n is experiment times
+M = 10;
+A = randi([0 M-1],1,n);
 
-hist(A,100)
+hist(A)
 
+X = hist(A);
+X_theo = repmat(n/M,1,M);
+ChisquaredTest = sum((X-X_theo).^2./X_theo);
+ChisquaredThreshold_95 = chi2inv(0.95,M);
+
+disp(['ChisquaredTest = ',num2str(ChisquaredTest),',	ChisquaredTest = ',num2str(ChisquaredThreshold_95)]);
+
+Y = [0 repmat(n/M,1,M-1)]
+ChisquaredTest = (X-Y).^2./Y%sum((X-Y).^2./Y);
+ChisquaredThreshold_95 = chi2inv(0.95,M);
+
+disp(['ChisquaredTest = ',num2str(ChisquaredTest),',	ChisquaredTest = ',num2str(ChisquaredThreshold_95)]);
+
+% use different n, infinite? 

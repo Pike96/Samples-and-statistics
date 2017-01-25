ad78c1b739094cb2c4f49b3cf5b93ae77656f4dc
Yucheng Lin <Pike96@users.noreply.github.com>
2017-01-25 01:22:44 -0800

    Change mean and var, formatting

--- Q1.m
+++ Q1.m
@@ -1,25 +1,23 @@
 clear;
-n = 10000;            % n is the number of the samples
+n = input('Please enter the number of samples: ');
 X = [];               % X stores the sample outcomes
-Sx2 = 0;              % Sx2 is the sum of sample variance
 
 for i = 1:n
-    X = [X 5*rand() - 3];   % Generate random number in [ - 3,2] and store in X
+    % Generate random number in [-3,2] and store in X
+    X = [X 5*rand() - 3];   
 end
 
 hist(X,100);
 
-disp(['The sample mean is: ',num2str(sum(X)/n)]);
-
-for i = 1:n
-    Sx2 = Sx2 + (X(i) - sum(X)/n)^2;
-end
-
-disp(['The sample variance is: ',num2str(Sx2/(n - 1))]);
+disp(['The sample mean is: ',num2str(mean(X))]);
+disp(['The sample variance is: ',num2str(var(X))]);
 
 M = sort(bootstrp(n,@mean,X));
 S = sort(bootstrp(n,@std,X));
 
 % Get index of 2.5% point and 97.5% point
-disp(['The bootstrap confidence interval for the sample mean is: [',num2str(M(round(n*0.025))),',',num2str(M(round(n*0.975))),']']);
-disp(['The bootstrap confidence interval for the sample variance is: [',num2str(S(round(n*0.025))),',',num2str(S(round(n*0.975))),']']);
+disp(['The bootstrap confidence interval for sample mean is: [',...
+    num2str(M(round(n*0.025))),',',num2str(M(round(n*0.975))),']']);
+disp(['The bootstrap confidence interval ',...
+    'for sample standard deviation is: [',...
+    num2str(S(round(n*0.025))),',',num2str(S(round(n*0.975))),']']);
